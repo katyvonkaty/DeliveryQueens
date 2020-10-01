87c8537c3c25f62b5ae1c32adddfb91bf798fe4e
katyvonkaty <kj0203@gmail.com>
2020-10-01 12:30:47 -0400

    Updates

---
+++ precache-manifest.e8a01dfe98f57c336aca23861daf6c2b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a5e581e7b946924921d94b7e9a083c",
+    "url": "/katyvonkaty/DeliveryQueens/index.html"
+  },
+  {
+    "revision": "a7a1c07164fb6ecae0f9",
+    "url": "/katyvonkaty/DeliveryQueens/static/css/2.a9a8be40.chunk.css"
+  },
+  {
+    "revision": "567f8b64f5909c0fc553",
+    "url": "/katyvonkaty/DeliveryQueens/static/css/main.4f12aca3.chunk.css"
+  },
+  {
+    "revision": "a7a1c07164fb6ecae0f9",
+    "url": "/katyvonkaty/DeliveryQueens/static/js/2.ab864f2f.chunk.js"
+  },
+  {
+    "revision": "582435898ec1da928b7fd86f88d2163a",
+    "url": "/katyvonkaty/DeliveryQueens/static/js/2.ab864f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567f8b64f5909c0fc553",
+    "url": "/katyvonkaty/DeliveryQueens/static/js/main.72f224b5.chunk.js"
+  },
+  {
+    "revision": "98548d8035a83989ebf1",
+    "url": "/katyvonkaty/DeliveryQueens/static/js/runtime-main.f98c1326.js"
+  },
+  {
+    "revision": "0363568e5fd61b4527986e405bfd900a",
+    "url": "/katyvonkaty/DeliveryQueens/static/media/cookies.0363568e.jpg"
+  },
+  {
+    "revision": "a6b4aa8abb4a7d819b855a5f6f35b10f",
+    "url": "/katyvonkaty/DeliveryQueens/static/media/item1.a6b4aa8a.jpg"
+  },
+  {
+    "revision": "92d9f7cb922bc4a993caef697e7735ec",
+    "url": "/katyvonkaty/DeliveryQueens/static/media/item2.92d9f7cb.jpg"
+  },
+  {
+    "revision": "6c1e99df555c979e28258dccbd8e1f18",
+    "url": "/katyvonkaty/DeliveryQueens/static/media/item3.6c1e99df.jpg"
+  },
+  {
+    "revision": "aa6a89ac0d03d4fe612cdb9dea5bfe54",
+    "url": "/katyvonkaty/DeliveryQueens/static/media/item4.aa6a89ac.jpg"
+  },
+  {
+    "revision": "b48f4c36c742783f83336b43cc15bc6e",
+    "url": "/katyvonkaty/DeliveryQueens/static/media/item5.b48f4c36.jpg"
+  },
+  {
+    "revision": "241765750cfdba596090ba65704f3118",
+    "url": "/katyvonkaty/DeliveryQueens/static/media/item6.24176575.jpg"
+  },
+  {
+    "revision": "958927fb908107f84d6b4b359a660f04",
+    "url": "/katyvonkaty/DeliveryQueens/static/media/item7.958927fb.jpg"
+  },
+  {
+    "revision": "31cb7c8efd7402962199adfc52481169",
+    "url": "/katyvonkaty/DeliveryQueens/static/media/item8.31cb7c8e.jpg"
+  }
+]);
